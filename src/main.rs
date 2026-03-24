4591d239a357969317f848de3e36648c751aa20d
Tejas <tskayande@gmail.com>
2026-03-24 17:21:35 +0530

    Program to find area for a rectangle.
    Printing Structs using [derive(Debug)].

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,23 @@
 
-// We can store data of type reference in structs that refere to object outside
-// the struct but for that we need to specify the object lifetime for that..
-// Thats why we kept username and email as Strings for now instead of &str.
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+#[derive(Debug)] // This will help us print this struct
+struct Dimentions {
+    width: usize,
+    height: usize,
 }
 
-// Tuple Structure
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
+fn get_area(dim: &Dimentions) -> usize {
+    dim.width * dim.height 
+}
 
+fn main() -> () {
 
-// A Struct with no fields
-// Unit-like structs can be useful when you need to implement a trait on some
-// type but don’t have any data that you want to store in the type itself
-struct UnitStruct;
-
-fn main() {
-
-    let mut user1 = User {
-        active: true,
-        username: String::from("tejas"),
-        email: String::from("tejas@mail.com"),
-        sign_in_count: 1,
+    let square = Dimentions {
+        width: 25,
+        height: 25
     };
 
-    let mut user2 = User {
-        email: String::from("another@mail.com"),
-        // This fillls the rest of the values. This should come at the very last
-        ..user1 
-    };
+    println!("Square {:?}" , square);
+    println!("Square {:#?}", square);
 
-    user1.email = String::from("Hello World!");
-
-    let c= Color(18, 18, 18);
-    let p= Point(55, 100, 82);
-
-    // destructing a point. Just like a tuple.
-    let Point(p, q, r) = p;
+    println!("Area of the square {} x {}: {}", square.width, square.height, get_area(&square));
 }
