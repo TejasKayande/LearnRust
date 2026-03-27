240861510db08bc5a6a4d6d19f62e19baaba475c
Tejas <tskayande@gmail.com>
2026-03-27 21:03:05 +0530

    Enums and Values associated with Enums
    creating enums and objects, adding values to enum types, adding methods
    over enums and evaluting based on values.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,68 @@
 
-struct Dimentions {
-    width: usize,
-    height: usize,
+enum IpAddrKind1 {
+    V4,
+    V6,
 }
 
-impl Dimentions {
-    fn area(&self) -> usize {
-        self.width * self.height
-    }
+struct IpAddr1 {
+    kind: IpAddrKind1,
+    address: String,
 }
 
+enum IpAddrKind2 {
+    // Store the value directly inside the enum option
+    V4(String),
+    V6(String),
+}
 
-// structs can have multiple impl blocks.
-impl Dimentions {
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(u32, u32, u32)
+}
 
-    fn create(&self, w: usize, h: usize) -> Self {
-        Self {
-            width: w,
-            height: h,
+// adding methods on Message.
+impl Message {
+    // handling Message type this way...
+    fn update(&self) -> String {
+        match self {
+            Message::Quit                                   => "Quittin".to_string(),
+            Message::Move {x, y}                => format!("Moving to {} {}", x, y),
+            Message::Write(st)                     => format!("Writing: {}", st),
+            Message::ChangeColor(r, g, b) => format!("Change Color: {} {} {}", r, g, b)
         }
     }
 }
 
 fn main() -> () {
 
-    let square = Dimentions {
-        width: 25,
-        height: 25
+    // Instead of doing it this way, Rust provides a way to store values inside
+    // ENUM option
+    let _local: IpAddr1 = IpAddr1 { 
+        kind: IpAddrKind1::V4, 
+        address: String::from("192.168.0.1")
     };
 
-    println!("Area of the square {} x {}: {}", square.width, square.height, square.area());
+    let my_local_ip = IpAddrKind2::V4(String::from("192.168.20.12"));
+    if let IpAddrKind2::V4(ip) = my_local_ip {
+        println!("{}", ip);
+    }
 
-    let mut square2: Dimentions = Dimentions { width: 0, height: 0 };
-    square2 = square2.create(30, 30);
+    let msg = Message::Quit;
+    println!("{}", msg.update());
 
-    println!("Area of the square {} x {}: {}", square2.width, square2.height, square2.area());
+    // There is no NULL in Rust, but to accomodate this they have an enum in the std lib
+    // Option<T> {
+    //     Node,
+    //     Some(T)
+    // };
+
+    // now we have the ability to set y to None, which essesntially means NULL
+    let y: Option<i8> = Some(5);
+
+    // In other words, you have to convert an Option<T> to a T before you can perform T
+    // operations with it. Generally, this helps catch one of the most common issues
+    // with null: assuming that something isn’t null when it actually is.
+    let c = 5 + y; // cant do integer + Option<i8>
 }
