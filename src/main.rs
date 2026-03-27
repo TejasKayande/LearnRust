695724be2c785f3a908e91ce3c0ffa60b037504c
Tejas <tskayande@gmail.com>
2026-03-27 21:54:33 +0530

    Handling enum values with let if and let else
    Default cases in match, handling only one enum case with if let,
    handling enum case with let else, let else in functions.

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,72 @@
-
-enum IpAddrKind1 {
-    V4,
-    V6,
-}
-
-struct IpAddr1 {
-    kind: IpAddrKind1,
-    address: String,
-}
-
-enum IpAddrKind2 {
-    // Store the value directly inside the enum option
-    V4(String),
-    V6(String),
-}
+use std::f32::consts::E;
 
 enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(u32, u32, u32)
+    Quit(u8), // u8 is the window id that needs to be quit.
+    Move,
+    Jump,
+    Sneak,
+    Sprint
 }
 
-// adding methods on Message.
-impl Message {
-    // handling Message type this way...
-    fn update(&self) -> String {
-        match self {
-            Message::Quit                                   => "Quittin".to_string(),
-            Message::Move {x, y}                => format!("Moving to {} {}", x, y),
-            Message::Write(st)                     => format!("Writing: {}", st),
-            Message::ChangeColor(r, g, b) => format!("Change Color: {} {} {}", r, g, b)
-        }
-    }
+fn handle_quit(msg: Message) -> String {
+
+    // we can use let else to match a single pattern and return early if it does not match.
+    // if the pattern matches, the value is assigned to the variable and we can
+    // use it in the rest of the function.
+
+    let Message::Quit(window_id) = msg else {
+        return String::from("The message was not Quit!");
+    };
+
+    // here because we did not return in the else block,
+    // we can use window_id in this scope.
+
+    format!("The window with ID {} was quit!", window_id)
 }
 
 fn main() -> () {
 
-    // Instead of doing it this way, Rust provides a way to store values inside
-    // ENUM option
-    let _local: IpAddr1 = IpAddr1 { 
-        kind: IpAddrKind1::V4, 
-        address: String::from("192.168.0.1")
+    // Very safe way to have NULL values.
+
+    let opt: Option<usize> = Option::None;
+    // let opt: Option<usize> = Option::Some(5);
+    // let opt: Option<usize> = Option::Some(15);
+
+    let sum: String = match opt {
+        Option::None             => String::from("The Value is None"),
+        Option::Some(val) => format!("{}", val)
     };
 
-    let my_local_ip = IpAddrKind2::V4(String::from("192.168.20.12"));
-    if let IpAddrKind2::V4(ip) = my_local_ip {
-        println!("{}", ip);
+    println!("{}", sum);
+
+    // match needs to have all the possible values coverd.
+    let number = 9;
+
+    let was_three = match number {
+
+        3 => true,
+        4 => false,
+        _other => false, // other is a value that number was, that did not match an cases.
+
+        // if we dont want to use the variable, we can use just an '_' to catch the default value.
+        _ => false
+    };
+
+    println!("{}", was_three);
+
+
+    // to match only a single pattern in we can use if let
+
+    if let Some(5) = opt {
+        println!("The number in Option<T> was 5");
+    } else if let None = opt {
+        println!("The number in Option<T> was None ");
+    } else {
+        println!("The number in Option<T> was not 5 or None");
     }
 
-    let msg = Message::Quit;
-    println!("{}", msg.update());
+    let msg = Message::Quit(5);
+    let result = handle_quit(msg);
+    println!("{}", result);
 
-    // There is no NULL in Rust, but to accomodate this they have an enum in the std lib
-    // Option<T> {
-    //     Node,
-    //     Some(T)
-    // };
-
-    // now we have the ability to set y to None, which essesntially means NULL
-    let y: Option<i8> = Some(5);
-
-    // In other words, you have to convert an Option<T> to a T before you can perform T
-    // operations with it. Generally, this helps catch one of the most common issues
-    // with null: assuming that something isn’t null when it actually is.
-    let c = 5 + y; // cant do integer + Option<i8>
 }
