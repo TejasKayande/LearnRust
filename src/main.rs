a66c69e93b6c5c3505bb28f622ecec054b3a756d
Tejas <tskayande@gmail.com>
2026-03-30 23:40:54 +0530

    Hash Map
    create hashmaps, get value by its key, loop over hashmap, entry and
    or_insert, program to count occurances of a word in a string.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,51 @@
 
-fn main() -> () {
-    
-    let mut _s = String::new(); // new empty string
+use std::collections::HashMap;
 
-    let mut s = String::from("Hello, world!");
-    println!("{}", s);
+fn count_word_occurrence(text: &str) -> HashMap<&str, usize> {
 
-    s.push_str(" Welcome to Rust programming.");
-    println!("{}", s);
+    let mut map: HashMap<&str, usize> = HashMap::new();
 
-    let litral = "This is a string literal.";
-    println!("{}", litral);
+    for word in text.split_whitespace() {
 
-    let litral_to_string = litral.to_string();
-    println!("{}", litral_to_string);
+        let count = map.entry(word).or_insert(0);
 
-    let hello = String::from("नमस्ते"); // supports Unicode characters
-    println!("{}", hello);
-
-    let s2 = s + &hello; // s was moved here 
-    println!("{}", s2);
-    // println!("{}", s); // because s was moved, this line would cause a compile error
-
-    // instead we could use format! macro to concatenate without moving ownership
-    let s3 = format!("{} {}", s2, hello);
-    println!("{}", s3);
-    println!("{}", s2); // We can still use s2 after the use of format! 
-
-    // you cannot index into a String with an integer because of UTF-8 encoding,
-    // which can have variable byte lengths for characters.
-    // s3[0] 
-
-    // iterating over characters in a string
-    for c in s3.chars() {
-        print!("{}", c);
+        // count is a reference to the value in the map, so we dereference it to update the count
+        *count += 1;
     }
-    println!();
 
-    // review previous commits for slicing strings and string literals 
+    map
+}
+
+fn main() -> () {
+
+    let mut hash_map = HashMap::new();
+
+    hash_map.insert(String::from("key1"), String::from("value1"));
+    hash_map.insert(String::from("key2"), String::from("value2"));
+    hash_map.insert(String::from("key3"), String::from("value3"));
+    hash_map.insert(String::from("key4"), String::from("value4"));
+    hash_map.insert(String::from("key5"), String::from("value5"));
+    hash_map.insert(String::from("key6"), String::from("value6"));
+    println!("{:#?}", hash_map);
+
+    // accessing values
+    let value1 = hash_map.get("key1");
+    let value1 = match value1 {
+        Some(value) => value,
+        None => "Key not found",
+    };
+    println!("Value for key1: {:?}", value1);
+
+    for (key, value) in &hash_map {
+        println!("{}: {}", key, value);
+    }
+
+    // to check if a key exists, if not we can use to_insert on entry to add
+    // that key with a value.
+    hash_map.entry(String::from("ke14")).or_insert(String::from("default_value"));
+    println!("{:#?}", hash_map);
+
+    let map = count_word_occurrence("This is my awsome and words list where I list awsome!");
+    println!("{:#?}", map);
 
 }
