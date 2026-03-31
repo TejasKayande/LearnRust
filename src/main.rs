0e27fa740c48d03d20716d71accd0b2428e226aa
Tejas <tskayande@gmail.com>
2026-03-31 12:28:45 +0530

    Error Handling and panic!()
    using panic!() to abort the program, using Result<T, E> to handle
    errors at runtime, io::ErrorKind, .unwrap() and .expect() on Result.

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,38 @@
 
-use std::collections::HashMap;
+use std::fs::File;
+use std::io::ErrorKind;
 
-fn count_word_occurrence(text: &str) -> HashMap<&str, usize> {
+fn main() {
 
-    let mut map: HashMap<&str, usize> = HashMap::new();
+    // handling errors with Result.
 
-    for word in text.split_whitespace() {
+    let _file = File::open("hello.txt");
 
-        let count = map.entry(word).or_insert(0);
-
-        // count is a reference to the value in the map, so we dereference it to update the count
-        *count += 1;
-    }
-
-    map
-}
-
-fn main() -> () {
-
-    let mut hash_map = HashMap::new();
-
-    hash_map.insert(String::from("key1"), String::from("value1"));
-    hash_map.insert(String::from("key2"), String::from("value2"));
-    hash_map.insert(String::from("key3"), String::from("value3"));
-    hash_map.insert(String::from("key4"), String::from("value4"));
-    hash_map.insert(String::from("key5"), String::from("value5"));
-    hash_map.insert(String::from("key6"), String::from("value6"));
-    println!("{:#?}", hash_map);
-
-    // accessing values
-    let value1 = hash_map.get("key1");
-    let value1 = match value1 {
-        Some(value) => value,
-        None => "Key not found",
+    let _file = match _file {
+        Ok(file) => {
+            println!("File opened successfully");
+            file
+        },
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => {
+                    println!("File created successfully");
+                    fc
+                },
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            _ => panic!("Problem opening the file: {:?}", error),
+        }
     };
-    println!("Value for key1: {:?}", value1);
 
-    for (key, value) in &hash_map {
-        println!("{}: {}", key, value);
-    }
+    // we can instead use unwrap method, which returns the value inside Ok if
+    // the result is Ok, but panics if the result is Err. We can also use expect
+    // method, which is similar to unwrap but allows us to specify the panic
+    // message.
+    let _file = File::open("test.txt").unwrap();
 
-    // to check if a key exists, if not we can use to_insert on entry to add
-    // that key with a value.
-    hash_map.entry(String::from("ke14")).or_insert(String::from("default_value"));
-    println!("{:#?}", hash_map);
-
-    let map = count_word_occurrence("This is my awsome and words list where I list awsome!");
-    println!("{:#?}", map);
+    // additionally we can use .expect() method to provide a custom error
+    // message if the file cannot be opened.
+    let _file = File::open("test.txt").expect("Failed to open test.txt");
 
 }
